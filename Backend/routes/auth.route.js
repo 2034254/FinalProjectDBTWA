465d8597e1b39f35033403fa2f64c709b315d565
2034254 <le2034254@crc-lennox.qc.ca>
2023-04-18 16:01:28 -0400

    Installed bcrypt, cors, mongodb, and morgan for the server.

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+
 const authRouter = require('./routes/auth.route');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms'));
+app.use(cors());
 
 app.use('/auth', authRouter);
 
