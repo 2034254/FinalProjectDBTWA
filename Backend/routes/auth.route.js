0e433cae137cdc53b4acf82094762b9937d624d9
2034254 <le2034254@crc-lennox.qc.ca>
2023-04-18 15:47:09 -0400

    Installed jsonwebtoken.

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
     res.status(200).send({
         message: 'Auth route is working.',
-    })
+    });
 });
 
 router.post('/register', (req, res) => {
