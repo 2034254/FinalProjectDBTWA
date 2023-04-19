9ee1a01805fed89e2643d6a5b4a9364d1add832c
2034254 <le2034254@crc-lennox.qc.ca>
2023-04-19 14:27:07 -0400

    Programmed register roue with possible errors.

--- Backend/routes/auth.route.js
+++ Backend/routes/auth.route.js
@@ -15,12 +15,27 @@ router.get('/', (req, res) => {
 });
 
 function ValidateEmail(email) {
-    const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    return validRegex.test(email)
+    const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    return emailRegex.test(email)
 }
+
+function ValidatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])[a-zA-Z\d]{8,}$/
+    return passwordRegex.test(password)
+  }
 
 router.post('/register', async (req, res) => {
     const body = req.body;
+
+    if (!body.username || !body.email || !body.password || !body.confirmPassword || body.username === '' || body.email === '' || body.password === '' || body.confirmPassword !== '' || body.acceptsTermsAndCoditions === false) {
+        res.status(400);
+        res.send({
+          message: "Please fill all fields and accept Terms and Conditions.",
+          body
+        });
+        
+        return;
+    }
 
     const email = await collection.findOne({
         email: body.email,
@@ -56,29 +71,31 @@ router.post('/register', async (req, res) => {
         return
     }
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        const passwordHash = await bcrypt.hash(body.password, salt);
-
-        const result = await collection.insertOne({
-
-            username: body.username,
-            email: body.email,
-            password: passwordHash,
-            dateOfBirth: body.dateOfBirth,
-            acceptsTermsAndCoditions: body.acceptsTermsAndCoditions
-        
+    if (!ValidatePassword(body.password)) {
+        res.status(400);
+        res.send({
+          message: "The password must have a minimum of 8 characters and contain upper and lower case letters",
         });
-
-        res.status(201).send({
-            status: 'Success.',
-            result
-        });
-    } catch {
-        res.status(400).send({
-            status: "Failed"
-        });
+        return;
     }
+
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(body.password, salt);
+
+    const result = await collection.insertOne({
+
+        username: body.username,
+        email: body.email,
+        password: passwordHash,
+        dob: body.dob,
+        acceptsTermsAndCoditions: body.acceptsTermsAndCoditions
+    
+    });
+
+    res.status(201).send({
+        status: 'Success.',
+        result
+    });
 });
 
 router.post('/login', (req, res) => {
