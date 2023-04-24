c023e01e07407d1f4eaad564232670799d332fa9
Gonzalo Erian Barrera Gonzalez <LE2129110@crc-lennox.qc.ca>
2023-04-24 15:54:18 -0400

    Draft of Graph almost completed

--- Frontend/my-auth-app/src/views/Graph.jsx
+++ Frontend/my-auth-app/src/views/Graph.jsx
@@ -3,12 +3,13 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
 import { useState } from 'react';
-import './graph.css';
+import './Graph.css';
 
 function Graph() {
 
     const [username, setUserName] = useState(null);
     const [isAdmin, setIsAdmin] = useState(false);
+    const [xyz, setImage] = useState("");
     const navigate = useNavigate();
 
     const handleImage = async (e) => {
@@ -25,11 +26,12 @@ function Graph() {
         if(response.status == 200) {
           const blob = await response.blob();
           const imgUrl = URL.createObjectURL(blob);
-          console.log('imgUrl: ', imgUrl);
+          //console.log('imgUrl: ', imgUrl);
     
           const img = new Image();
           img.src = imgUrl;
-          document.body.appendChild(img);
+          setImage(imgUrl)
+          //document.body.appendChild(img);
         }
     }
 
@@ -54,61 +56,95 @@ function Graph() {
 
     return(
         <div>
-            <h1>Home</h1>
+            <h1>Graph</h1>
             <button className='btn btn-success'  onClick={handleImage}>
-          
-            MAPA
-        </button>
-            
-            <nav className='navbar navbar-dark bg-dark topnav'>
-                <label>Data Explorer</label>
-                <div className='btn-group'>
-                    <h2>Gas or Warming</h2>
-                    <button className='btn btn-secondary dropdown-toggle' type='button' id='defaultDropdown' data-bs-toggle='dropdown' data-bs-auto-close='true' aria-expanded='false'>
-                        C02    
-                    </button>
-                    
-                    <ul className='dropdown-menu' aria-labelledby='defaultDropdown'>
-                        <li><a className='dropdown-item' href='#'>C02</a></li>
-                        <li><a className='dropdown-item' href='#'>All GHGs</a></li>
-                        <li><a className='dropdown-item' href='#'>Methane</a></li>
-                        <li><a className='dropdown-item' href='#'>Nitrous Oxide</a></li>
+                MAP
+            </button>
+        <div className="container-flex">
+            <div className="row my-2">
+                <div className="col">
+                    <div className='btn-group buttonDropdown'>
+                        <button className='btn btn-secondary dropdown-toggle' type='button' id='defaultDropdown' data-bs-toggle='dropdown' data-bs-auto-close='true' aria-expanded='false'>
+                            C02    
+                        </button>
+                        <ul className='dropdown-menu' aria-labelledby='defaultDropdown'>
+                            <li><a className='dropdown-item' href='#'>C02</a></li>
+                            <li><a className='dropdown-item' href='#'>All GHGs</a></li>
+                            <li><a className='dropdown-item' href='#'>Methane</a></li>
+                            <li><a className='dropdown-item' href='#'>Nitrous Oxide</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="col order-5 d-flex">
+                    <div className="form-check align-self-center">
+                        <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault" />
+                        <label className="form-check-label" htmlFor="flexCheckDefault">
+                            Relative to world total
+                        </label>
+                    </div>
+                </div>
+                <div className="col order-1">
+                    <div className='btn-group buttonDropdown'>
+                        <button className='btn btn-secondary dropdown-toggle' type='button' id='defaultDropdown' data-bs-toggle='dropdown' data-bs-auto-close='true' aria-expanded='false'>
+                            Per capita
+                        </button>
+                        <ul className='dropdown-menu' aria-labelledby='defaultDropdown'>
+                            <li><a className='dropdown-item' href='#'>Per capita</a></li>
+                            <li><a className='dropdown-item' href='#'>Per country</a></li>
+                            <li><a className='dropdown-item' href='#'>Cumulative</a></li>
+                            <li><a className='dropdown-item' href='#'>Per MWh of Energy</a></li>
+                            <li><a className='dropdown-item' href='#'>Per $ of GDP</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div className='row my-2'>
+                <div className='col col-2 buttonDropdown'>
+                    <ul className='text-left'>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Argentina</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Australia</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Brazil</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Canada</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />China</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />France</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Germany</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />India</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Indonesia</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Japan</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Mexico</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Russia</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />Ukraine</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />United Kingdom</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />United States</li>
+                        <li className='checkBoxHigh text-left'><input type="checkbox" />World</li>
                     </ul>
                 </div>
-                <div className='btn-group'>
-                    <h2>Count</h2>
-                    <button className='btn btn-secondary dropdown-toggle' type='button' id='defaultDropdown' data-bs-toggle='dropdown' data-bs-auto-close='true' aria-expanded='false'>
-                        Per capita
-                    </button>
-                    <ul className='dropdown-menu' aria-labelledby='defaultDropdown'>
-                        <li><a className='dropdown-item' href='#'>Per capita</a></li>
-                        <li><a className='dropdown-item' href='#'>Per country</a></li>
-                        <li><a className='dropdown-item' href='#'>Cumulative</a></li>
-                        <li><a className='dropdown-item' href='#'>Per MWh of Energy</a></li>
-                        <li><a className='dropdown-item' href='#'>Per $ of GDP</a></li>
-                    </ul>
+                <div className='col col-9 d-flex'>
+                    <span className='align-self-center'>
+                        <img src={xyz} alt="" className='imageSize'/>
+                    </span>
                 </div>
-            </nav>
-            <nav className='sidebar'>
-                <ul>
-                    <li><input type="checkbox" />Argentina</li>
-                    <li><input type="checkbox" />Australia</li>
-                    <li><input type="checkbox" />Brazil</li>
-                    <li><input type="checkbox" />Canada</li>
-                    <li><input type="checkbox" />China</li>
-                    <li><input type="checkbox" />France</li>
-                    <li><input type="checkbox" />Germany</li>
-                    <li><input type="checkbox" />India</li>
-                    <li><input type="checkbox" />Indonesia</li>
-                    <li><input type="checkbox" />Japan</li>
-                    <li><input type="checkbox" />Mexico</li>
-                    <li><input type="checkbox" />Russia</li>
-                    <li><input type="checkbox" />Ukraine</li>
-                    <li><input type="checkbox" />United Kingdom</li>
-                    <li><input type="checkbox" />United States</li>
-                    <li><input type="checkbox" />World</li>
-                </ul>
-            </nav>
+            </div>
+            <div className='row my-2'>
+                <div className='col col-3'>
+                    <div typeof='button' className='btn btn-primary regularButton'>
+                        Generate Graph
+                    </div>
+                </div>
+                <div className='col col-2'></div>
+                <div className='col col-2'>
+                    <div typeof='button' className='btn btn-primary regularButton'>
+                        Save
+                    </div>
+                </div>
+                <div className='col col-2'>
+                    <div typeof='button' className='btn btn-primary regularButton'>
+                        Download
+                    </div>
+                </div>
+                <div className='col col-2'></div>
+            </div>
+        </div>
         </div>
     )
 }
