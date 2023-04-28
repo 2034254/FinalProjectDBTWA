2e078256dd3b315fedb742f641b599aa05cc3f27
2034254 <le2034254@crc-lennox.qc.ca>
2023-04-28 10:31:45 -0400

    Fixed jsonwebtoken check on Home.jsx and cleaned up some code.

--- Frontend/my-auth-app/src/views/Home.jsx
+++ Frontend/my-auth-app/src/views/Home.jsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import { useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState} from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import jwt_decode from 'jwt-decode';
-import { useState } from 'react';
 import './home.css';
-import { Link } from 'react-router-dom'
 
 function Home() {
 
-    const [count, setCount] = useState(0)
     const [username, setUserName] = useState(null);
-    const [isAdmin, setIsAdmin] = useState(false);
     const navigate = useNavigate();
 
     // On component load -> check auth
@@ -22,8 +17,12 @@ function Home() {
             return
         } try {
             const decodedToken = jwt_decode(token);
+            const currentTime = Date.now() / 1000;
+            if (decodedToken.exp < currentTime) {
+                navigate('/login');
+                return;
+            }
             setUserName(decodedToken.username);
-            setIsAdmin(decodedToken.isAdmin);
         } catch(err) {
             console.error(err);
             navigate('/login');
@@ -31,15 +30,25 @@ function Home() {
         }
     }, [])
 
+    const handleLogout = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    }
+
     return(
         <div>
             <h1>Home</h1>
             Welcome {username}. You are logged in!
-            {isAdmin? 'You are an admin btw.':''}
-            <button className='btn btn-success' onClick={() => setCount((count) => count + 1)}>
+            
+            <div> 
                 <Link to='/graph'>
-                    Graph
-                </Link>
+                    <button className='btn btn-success'>
+                        Graph
+                    </button>
+                </Link>   
+            </div> 
+            <button className='btn btn-success' onClick={handleLogout}>
+                Logout
             </button>
         </div>
     )
