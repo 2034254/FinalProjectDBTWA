f4c5af9812c65cd61cbe18cf8593e5e5921453c9
vlad <estevez.vladimir@yahoo.com>
2023-05-01 15:03:53 -0400

    added the route for the consult button

--- Frontend/my-auth-app/src/views/Home.jsx
+++ Frontend/my-auth-app/src/views/Home.jsx
@@ -35,6 +35,36 @@ function Home() {
         navigate('/login');
     }
 
+
+    const handleSave = async (e) => {
+        e.preventDefault();
+    
+        const loginURL = 'http://localhost:8080/file/file';
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'authorization': localStorage.getItem('token')
+            },
+            body: JSON.stringify({
+                countries: selectedCountries,
+                graphType: selectedGraph,
+            }),
+    
+        }
+        const response = await fetch(loginURL, options);
+        console.log('response.status: ', response.status);
+    
+        if (response.status == 200) {
+            const blob = await response.blob();
+            const imgUrl = URL.createObjectURL(blob);
+            //console.log('imgUrl: ', imgUrl);
+    
+            // Open the image in a new tab
+            window.open(imgUrl);
+        }
+    }
+
     return(
         <div>
             <h1>Home</h1>
@@ -50,7 +80,7 @@ function Home() {
             <button className='btn btn-success' onClick={handleLogout}>
                 Logout
             </button>
-        </div>
+            </div>
     )
 }
 
