8e02b7ed3e4bdf8e24d7d5ed71bce8bca0f9d787
Gonzalo Barrera <LE2129110@crc-lennox.qc.ca>
2023-05-02 13:23:52 -0400

    Correcting Register measures

--- Frontend/my-auth-app/src/views/Register.jsx
+++ Frontend/my-auth-app/src/views/Register.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import jwt_decode from 'jwt-decode';
 import 'react-toastify/dist/ReactToastify.css';
+import greenLogo2 from '../assets/greenLab2.jpg';
+import './Register.css';
 const toastOptions = {
     position: "top-right",
     autoClose: 5000,
@@ -72,35 +74,60 @@ function Register() {
 
     return(
         <div>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label className='form-label' htmlFor="username">Username:</label>
-                    <input className='form-control' type="username" id="username" required onChange={(event) => handleInputChange('username', event.target.value)}/>
-                </div>
-                <div>
-                    <label className='form-label' htmlFor="email">Email:</label>
-                    <input className='form-control' type="email" id="email" required onChange={(event) => handleInputChange('email', event.target.value)}/>
-                </div>
+            <div className='container'>
+                <span className='align-self-center' >
+                    <img src={greenLogo2} alt="Green Lab2" className='' />
+                </span>
+                <h1>Register</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className='row g-3 justify-content-center'>
+                        <div className='col-auto'>
+                            <label className='form-label' htmlFor="username">Username:</label>
+                            <input className='form-control' type="username" id="username" required onChange={(event) => handleInputChange('username', event.target.value)}/>
+                        </div>
+                    </div>
+                    <div className='row g-3 justify-content-center'>
+                        <div className='col-auto'>
+                            <label className='form-label' htmlFor="email">Email:</label>
+                            <input className='form-control' type="email" id="email" required onChange={(event) => handleInputChange('email', event.target.value)}/>
+                        </div>
+                    </div>
+                    <div className='row g-3 justify-content-center'>
+                        <div className='col-auto'>
+                            <br />
+                                <label className='form-label' htmlFor="password">Password:</label>
+                                <input className='form-control' type="password" id="password" required onChange={(event) => handleInputChange('password', event.target.value)}/>
+                        </div>
+                    </div>
+                    <div className='row g-3 justify-content-center'>
+                        <div className='col-auto'>
+                            <br />
+                                <label className='form-label' htmlFor="confirmPassword">Confirm:</label>
+                                <input className='form-control' type="password" id="confirmPassword" required onChange={(event) => handleInputChange('confirmPassword', event.target.value)}/>
+                        </div>
+                    </div>
+                    <div className='row g-3 justify-content-center'>
+                        <div className='col-auto'>
+                            <br />
+                                <label className='form-label' htmlFor="dob">DOB:</label>
+                                <input className='form-control' type="date" id="dob" required onChange={(event) => handleInputChange('dob', event.target.value)}/>
+                        </div>
+                    </div>
+                    <div className='row g-3 justify-content-center'>
+                        <div className='col-auto'>
+                            <br />
+                                <label className='form-label' htmlFor="terms">Terms:</label>
+                                <input className='m-2 form-check-input' type="checkbox" id="terms" required onChange={(event) => handleInputChange('terms', event.target.checked)}/>
+                        </div>
+                    </div>
+                    <br />
+                        <button type="submit">Save</button>
+                </form>
                 <br />
-                <label className='form-label' htmlFor="password">Password:</label>
-                <input className='form-control' type="password" id="password" required onChange={(event) => handleInputChange('password', event.target.value)}/>
-                <br />
-                <label className='form-label' htmlFor="confirmPassword">Confirm:</label>
-                <input className='form-control' type="password" id="confirmPassword" required onChange={(event) => handleInputChange('confirmPassword', event.target.value)}/>
-                <br />
-                <label className='form-label' htmlFor="dob">DOB:</label>
-                <input className='form-control' type="date" id="dob" required onChange={(event) => handleInputChange('dob', event.target.value)}/>
-                <br />
-                <label className='form-label' htmlFor="terms">Terms:</label>
-                <input className='m-2 form-check-input' type="checkbox" id="terms" required onChange={(event) => handleInputChange('terms', event.target.checked)}/>
-                <br />
-                <button type="submit">Save</button>
-            </form>
-            <br />
-            <button onClick={backToLogin}>
-                Login? Click Here
-            </button>
+                    <button onClick={backToLogin}>
+                        Login? Click Here
+                    </button>
+            </div>
             <ToastContainer />
         </div>
     )
